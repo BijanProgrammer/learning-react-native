7c2c14ad56563ef17a4475827c54cea9b64e8cae
BijanProgrammer <bijaneisapour@gmail.com>
2021-03-23 17:01:29 +0430

    [EXPERIMENT] FlatList

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
 import React, {useState} from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
     const [people, setPeople] = useState([
-        {id: 0, name: 'Bijan'},
-        {id: 1, name: 'Samin'},
-        {id: 2, name: 'Reza'},
+        {id: '0', name: 'Bijan'},
+        {id: '1', name: 'Samin'},
+        {id: '2', name: 'Reza'},
     ]);
     
     return (
         <View style={styles.container}>
-            <ScrollView>
-                {
-                    people.map(person => (
-                        <View key={person.id}>
-                            <Text style={styles.person}>{person.name}</Text>
-                        </View>
-                    ))
-                }
-            </ScrollView>
+            <FlatList data={people} renderItem={({item: person}) => (
+                <Text style={styles.person}>{person.name}</Text>
+            )}/>
         </View>
     );
 }
