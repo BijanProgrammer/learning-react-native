bf91e504b4ccc41b08d80105cb3a5252cac9f60b
BijanProgrammer <bijaneisapour@gmail.com>
2021-03-23 16:40:24 +0430

    [EXPERIMENT] TextInput

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [name, setName] = useState('Bijan');
+    const [age, setAge] = useState(20);
+    
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Hello, {name}!</Text>
+            <Text style={styles.text}>You are {age} years old!</Text>
+            <TextInput style={[styles.text, styles.input]} palceholder="e.g. John Doe" placeholderTextColor="#fafafa"
+                       onChangeText={(value) => setName(value)}/>
+            <StatusBar style="light"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#1a1a1a',
+        color: '#fafafa',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: '#fafafa',
+    },
+    input: {
+        width: 200,
+        marginTop: 20,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#fafafa',
+    },
 });
